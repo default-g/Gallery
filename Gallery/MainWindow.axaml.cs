0df9b328bd16b5bc45cabd69378e0ae491be421c
Artem <arteam_101@mail.ru>
2022-04-08 15:39:51 +0700

    Добавьте файлы проекта.

---
+++ Gallery/MainWindow.axaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using Avalonia.VisualTree;
+using TreeDataGridDemo.Models;
+using TreeDataGridDemo.ViewModels;
+
+namespace TreeDataGridDemo
+{
+    public class MainWindow : Window
+    {
+        private readonly TabControl _tabs;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.AttachDevTools();
+            Renderer.DrawFps = true;
+            DataContext = new MainWindowViewModel();
+
+            _tabs = this.FindControl<TabControl>("tabs");
+
+            DispatcherTimer.Run(() =>
+            {
+                UpdateRealizedCount();
+                return true;
+            }, TimeSpan.FromMilliseconds(500));
+
+            Activated += MainWindow_Activated;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void MainWindow_Activated(object? sender, EventArgs e)
+        {
+            Program.Stopwatch!.Stop();
+            System.Diagnostics.Debug.WriteLine("Startup time: " + Program.Stopwatch.Elapsed);
+        }
+
+        private void SelectedPath_KeyDown(object? sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var vm = (MainWindowViewModel)DataContext!;
+                vm.Files.SelectedPath = ((TextBox)sender!).Text;
+            }
+        }
+
+        private void UpdateRealizedCount()
+        {
+            var tabItem = (TabItem)_tabs.SelectedItem!;
+            var treeDataGrid = (TreeDataGrid)((Control)tabItem.Content).GetLogicalDescendants()
+                .First(x => x is TreeDataGrid tl);
+            var textBlock = (TextBlock)((Control)tabItem.Content).GetLogicalDescendants()
+                .First(x => x is TextBlock tb && tb.Classes.Contains("realized-count"));
+            var rows = treeDataGrid.RowsPresenter!;
+            var realizedRowCount = rows.RealizedElements.Count;
+            var unrealizedRowCount = ((ILogical)rows).LogicalChildren.Count - realizedRowCount;
+            textBlock.Text = $"{realizedRowCount} rows realized ({unrealizedRowCount} unrealized)";
+        }
+    }
+}
