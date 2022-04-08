9053575b95d94c843a3f60e018ea679c7fb94050
Artem <arteam_101@mail.ru>
2022-04-08 15:57:54 +0700

    Splitted view

--- Gallery/MainWindow.axaml.cs
+++ Gallery/MainWindow.axaml.cs
@@ -20,17 +20,8 @@ namespace TreeDataGridDemo
         public MainWindow()
         {
             InitializeComponent();
-            this.AttachDevTools();
-            Renderer.DrawFps = true;
+           
             DataContext = new MainWindowViewModel();
-
-            _tabs = this.FindControl<TabControl>("tabs");
-
-            DispatcherTimer.Run(() =>
-            {
-                UpdateRealizedCount();
-                return true;
-            }, TimeSpan.FromMilliseconds(500));
 
             Activated += MainWindow_Activated;
         }
@@ -42,8 +33,7 @@ namespace TreeDataGridDemo
 
         private void MainWindow_Activated(object? sender, EventArgs e)
         {
-            Program.Stopwatch!.Stop();
-            System.Diagnostics.Debug.WriteLine("Startup time: " + Program.Stopwatch.Elapsed);
+           
         }
 
         private void SelectedPath_KeyDown(object? sender, KeyEventArgs e)
@@ -55,17 +45,6 @@ namespace TreeDataGridDemo
             }
         }
 
-        private void UpdateRealizedCount()
-        {
-            var tabItem = (TabItem)_tabs.SelectedItem!;
-            var treeDataGrid = (TreeDataGrid)((Control)tabItem.Content).GetLogicalDescendants()
-                .First(x => x is TreeDataGrid tl);
-            var textBlock = (TextBlock)((Control)tabItem.Content).GetLogicalDescendants()
-                .First(x => x is TextBlock tb && tb.Classes.Contains("realized-count"));
-            var rows = treeDataGrid.RowsPresenter!;
-            var realizedRowCount = rows.RealizedElements.Count;
-            var unrealizedRowCount = ((ILogical)rows).LogicalChildren.Count - realizedRowCount;
-            textBlock.Text = $"{realizedRowCount} rows realized ({unrealizedRowCount} unrealized)";
-        }
+     
     }
 }
